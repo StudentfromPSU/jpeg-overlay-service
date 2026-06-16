49374d7f1b67ab6bbbb27b9266395da7ef2adc03
Stanislau Chaiko <stanislau.chaiko@gmail.com>
2026-06-16 14:57:35 +0300

    fix config

--- include/config/config.hpp
+++ include/config/config.hpp
@@ -2,28 +2,6 @@
 
 #include <string>
 
-// Read environment variable or return default value
-inline std::string get_env(const std::string& key, const std::string& default_value = "")
-{
-#ifdef _WIN32
-    char* buffer = nullptr;
-    size_t size = 0;
-
-    if (_dupenv_s(&buffer, &size, key.c_str()) == 0 &&
-        buffer != nullptr)
-    {
-        std::string value(buffer);
-        free(buffer);
-        return value;
-    }
-
-    return default_value;
-#else
-    const char* value = std::getenv(key.c_str());
-    return value ? value : default_value;
-#endif
-}
-
 struct Config
 {
     std::string host;
@@ -32,8 +10,8 @@ struct Config
     static Config New()
     {
         Config config;
-        config.host = get_env("HOST", "0.0.0.0");
-        config.port = get_env("PORT", "50051");
+        config.host = "0.0.0.0";
+        config.port = "50051";
         return config;
     }
 };
